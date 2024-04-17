f063a13787835902dcb5498873f455ee8e793741
Irina Svetlova <irasvet1997@gmail.com>
2024-04-17 14:06:47 +0300

    add librarys, made folders

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
+// import userData from '../../../userData.json';
+
+// import css from './App.module.css';
+
+{/* <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+  <SearchBox />
+  <ContactList />
+</div> */}
